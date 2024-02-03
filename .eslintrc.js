ae802fbfcb811b4d632f7abe8594ebc76b68f67f
Sadjed <77120200+sajeed11@users.noreply.github.com>
2024-02-03 21:05:52 +0100

    Disable no-unused-vars rule and update ExploreCard styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
+    "no-unused-vars": "off",
     "react/no-unescaped-entities": 0,
     "eslintreact/no-danger": 0,
     "react/jsx-max-props-per-line": 0,
--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -7,21 +7,21 @@ import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div variants={fadeIn('right', 'spring', index * 0.5, 0.75)} className={`relative ${
-    active === id ? 'lg:flex-[3.5] flex -[10] ': 'lg:flex-[0.5] flex-[2] ' 
-    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    active === id ? 'lg:flex-[3.5] flex-[13]': 'lg:flex-[0.5] flex-[2] ' 
+    } flex items-center justify-center lg:min-w-1/4 w-full h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
   onClick={() => handleClick(id)}>
-    <img src={imgUrl} alt={title} className='absolute w-full h-full object-cover rounded-[24px]' />
+    <img src={imgUrl} alt={title} className='absolute w-full h-full object-cover rounded-2xl'/>
     {active !== id ? (
-      <h3 className='font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]'>
+      <h3 className='font-semibold sm:text-xl text-lg text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0] transition-all'>
         {title}
       </h3>
     ) : (
-    <div className='absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]'>
-      <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism-mb-[16px]`}>
+    <div className='absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-2xl'>
+      <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-2xl`}>
         <img src='/headset.svg' alt='headset' className='w-1/2 h-1/2 object-contain'/>
-          </div>
-          <p className='font-normal text-[16px] leading-[20px] text-white uppercase'>Enter the Metaverse</p>
-          <h2 className='mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white'>{title}</h2>
+      </div>
+          <p className='font-normal text-base text-white uppercase'>Enter the Metaverse</p>
+          <h2 className='mt-6 font-semibold sm:text-3xl text-2xl text-white'>{title}</h2>
     </div>
     )}
   </motion.div>
