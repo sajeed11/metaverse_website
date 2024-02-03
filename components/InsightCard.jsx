67c85142b8a11dd924a24235bf43a827415cd7ed
Sadjed <77120200+sajeed11@users.noreply.github.com>
2024-02-03 21:12:06 +0100

    Refactor InsightCard component and add animations

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,9 +1,22 @@
 'use client';
 
-const InsightCard = () => (
-  <div>
-    Insight Card
-  </div>
+import { motion } from 'framer-motion';
+import { fadeIn } from '../utils/motion';
+
+const InsightCard = ({imgUrl, title, subtitle, index}) => (
+  <motion.div variants={fadeIn('up', 'spring', index * 0.5, 1)} className='flex md:flex-row flex-col gap-4'>
+    <img src={imgUrl} alt='plant-01' className='md:w-72 w-full h-64 rounded-[32px] object-cover' />
+    <div className='w-full flex justify-between items-center'>
+      <div className='flex-1 md:ml-16 flex flex-col max-w-[650px]'>
+        <h4 className='text-white font-normal lg:text-5xl text-2xl'>{title}</h4>
+        <p className='text-secondary-white mt-4 lg:text-xl text-sm'>{subtitle}</p>
+      </div>
+
+      <div className='lg:flex hidden items-center justify-center w-24 h-24 rounded-full bg-transparent border-white border-[1px]'>
+        <img src='arrow.svg' alt='arrow' className='w-2/5 h-2/5 object-contain'/>
+      </div>
+    </div>
+  </motion.div>
 );
 
 export default InsightCard;
--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,24 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer} from '../utils/motion';
+import { InsightCard, TitleText, TypingText } from '../components';
+import { insights } from '../constants';
+
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings} relative z-10`}>
+    {/* <div className='gradient-02 z-0' /> */}
+    <motion.div variants={staggerContainer} initial='hidden' whileInView='show' viewport={{ once: false, amount: 0.25 }} className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}>
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText title="Insights to help you improve your game" textStyles="text-center" />
+      <div className='mt-[50px] flex flex-col gap-[30px]'>
+        {insights.map((insight, index) => (
+          <InsightCard key={`insight-${index}`} {...insight} index={ index + 1} />
+        ))}
+      </div>
+    </motion.div>
   </section>
 );
 
